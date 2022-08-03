f52a604ee1f94d9095a16d906654b4b47efab43d
César Hilário <cesar.hilario.dev@gmail.com>
2022-08-03 01:23:29 -0300

    chore: remove specific device int the preview of the WeatherView

--- WeatherIOS/WeatherIOS/Views/WeatherView.swift
+++ WeatherIOS/WeatherIOS/Views/WeatherView.swift
@@ -96,6 +96,6 @@ struct WeatherView: View {
 
 struct WeatherView_Previews: PreviewProvider {
     static var previews: some View {
-        WeatherView(weather: previewWeather).previewDisplayName("iPhone SE")
+        WeatherView(weather: previewWeather)
     }
 }
